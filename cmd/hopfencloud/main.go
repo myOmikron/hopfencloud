d56e8db4aabae22e56010e42c3d6a675fd3a2048
myOmikron <git@omikron.dev>
2022-06-05 22:02:20 +0200

    Added commands to reload and stop the server

--- cmd/hopfencloud/main.go
+++ cmd/hopfencloud/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"syscall"
 
 	"github.com/myOmikron/hopfencloud/server"
 
@@ -10,12 +13,38 @@ import (
 )
 
 func main() {
-	parser := argparse.NewParser("hopfencloud", "", &argparse.ParserConfig{
+	parser := argparse.NewParser("hopfencloud", "", &argparse.ParserConfig{})
+	startParser := parser.AddCommand("start", "Starts the server", &argparse.ParserConfig{
 		DisableDefaultShowHelp: true,
 	})
-
-	defaultConf := parser.String("", "config-path", &argparse.Option{
+	defaultConf := startParser.String("", "config-path", &argparse.Option{
 		Default: "/etc/hopfencloud/config.toml",
+	})
+
+	reloadParser := parser.AddCommand("reload", "Sends SIGHUP the the given process", nil)
+	reloadID := reloadParser.Int("", "process-id", &argparse.Option{
+		Required:   true,
+		Positional: true,
+		Help:       "Process ID",
+		Validate: func(arg string) error {
+			if i, _ := strconv.Atoi(arg); i <= 0 {
+				return errors.New("int must be > 0")
+			}
+			return nil
+		},
+	})
+
+	stopParser := parser.AddCommand("stop", "Sends SIGINT to the given process", nil)
+	stopID := stopParser.Int("", "process-id", &argparse.Option{
+		Required:   true,
+		Positional: true,
+		Help:       "Process ID",
+		Validate: func(arg string) error {
+			if i, _ := strconv.Atoi(arg); i <= 0 {
+				return errors.New("int must be > 0")
+			}
+			return nil
+		},
 	})
 
 	if err := parser.Parse(nil); err != nil {
@@ -23,5 +52,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	server.StartServer(*defaultConf)
+	switch {
+	case startParser.Invoked:
+		server.StartServer(*defaultConf)
+	case reloadParser.Invoked:
+		if process, err := os.FindProcess(*reloadID); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		} else {
+			if err := process.Signal(syscall.SIGHUP); err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+		}
+	case stopParser.Invoked:
+		if process, err := os.FindProcess(*stopID); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		} else {
+			if err := process.Signal(syscall.SIGINT); err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+		}
+	}
 }
