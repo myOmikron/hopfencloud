db60e848c4dc484dfc9cd4c7e647193332644ad7
myOmikron <git@omikron.dev>
2022-06-08 01:08:15 +0200

    Added checks to verify that Files.DataPath is writeable

--- models/conf/config.go
+++ models/conf/config.go
@@ -1,5 +1,11 @@
 package conf
 
+import (
+	"errors"
+	"os"
+	"path"
+)
+
 type Database struct {
 	Driver   string
 	Name     string
@@ -32,5 +38,29 @@ type Config struct {
 }
 
 func (c *Config) CheckConfig() error {
+	if c.Files.DataPath == "" {
+		return errors.New("invalid Files.DataPath setting")
+	}
+
+	if stat, err := os.Stat(c.Files.DataPath); err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(c.Files.DataPath, 0700); err != nil {
+				return err
+			}
+		}
+	} else {
+		if !stat.IsDir() {
+			return errors.New("data directory is a file")
+		}
+
+		if _, err := os.OpenFile(path.Join(c.Files.DataPath, "test"), os.O_CREATE, 0600); err != nil {
+			return err
+		}
+
+		if err := os.Remove(path.Join(c.Files.DataPath, "test")); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
