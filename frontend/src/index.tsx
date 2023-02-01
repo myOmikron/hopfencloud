a64449e7b818a171b696e05462fe8bc06bef6a12
myOmikron <git@omikron.dev>
2023-02-01 04:12:18 +0100

    Added toastify

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 type RouterProps = {};
 type RouterState = {};
 
 class Router extends React.Component<RouterProps, RouterState> {
-  constructor(props: RouterProps) {
-    super(props);
+    constructor(props: RouterProps) {
+        super(props);
 
-    this.state = {};
-  }
+        this.state = {};
+    }
 
-  render() {
-    return <div></div>;
-  }
+    render() {
+        return <div></div>;
+    }
 }
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <>
-    <Router />
-  </>
+    <>
+        <Router />
+        <ToastContainer />
+    </>
 );
